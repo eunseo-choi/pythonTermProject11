508d57fabbb7e790743683e25ac758ff67ce682a
eunseo-choi <68843219+eunseo-choi@users.noreply.github.com>
2021-05-27 20:42:50 +0900

    Tkinter UI

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import font
 from tkinter import scrolledtext
 import tkinter.ttk
 import tkinter.messagebox
+from xml.etree import ElementTree
 
 window = Tk()
 window.geometry("500x600")
@@ -24,11 +25,6 @@ class MainGUI:
         MainText.pack()
         MainText.place(x=20)
 
-    def InitRenderText(self):
-        self.RenderText = scrolledtext.ScrolledText(self.frame1)
-        self.RenderText.config(width=480,height=200)
-        self.RenderText.configure(state='disabled')
-
     def InitCharging(self):
         self.frame1 = Frame(window)
         self.notebook.add(self.frame1, text="전기차 충전소")
@@ -39,9 +35,13 @@ class MainGUI:
         self.SearchButton1 = Button(self.frame1, text="검색",command=self.SearchCharging)
         self.SearchButton1.pack()
         self.SearchButton1.place(x=210, y=30, width=40, height=30)
-        self.listNodes1 = Listbox(self.frame1)
-        self.listNodes1.pack()
-        self.listNodes1.place(x=10, y=70, width=480, height=200)
+
+        self.RenderTextScrollbar = Scrollbar(self.frame1)
+        self.RenderTextScrollbar.pack()
+        self.RenderTextScrollbar.place(x=480, y=70)
+        self.RenderText = Text(self.frame1)
+        self.RenderText.pack()
+        self.RenderText.place(x=10, y=70)
 
 
     def SearchCharging(self):
@@ -53,6 +53,7 @@ class MainGUI:
 
         ChdataList = []
         ChdataList.clear()
+        index = 0
 
         if req.status == 200:
             CharStationDoc = req.read().decode("utf-8")
@@ -60,48 +61,57 @@ class MainGUI:
                 print("ERROR")
             else:
                 parseData = parseString(CharStationDoc)
-                GeoInfoCharStation = parseData.childNodes
-                row = GeoInfoCharStation[0].childNodes
+                all = parseData.childNodes
+                body = all[0].childNodes
+                items = body[1].childNodes
+                item = items[0].childNodes
 
-                for item in row:
-                    if item.nodeName == "row":
-                        subitems = item.childNodes
-                        if subitems[0].firstChild.nodeValue in self.Label1.get():
-                            ChdataList.append(subitems[0].firstChild.nodeValue)
-                            if subitems[3].firstChild.nodeValue == 1:
-                                ChdataList.append("완속")
-                                print("완속")
-                            elif subitems[3].firstChild.nodeValue == 2:
-                                ChdataList.append("급속")
-                            elif subitems[9].firstChild.nodeValue == 1:
-                                ChdataList.append("충전가능")
-                            elif subitems[9].firstChild.nodeValue == 2:
-                                ChdataList.append("충전중")
-                            elif subitems[9].firstChild.nodeValue == 3:
-                                ChdataList.append("고장/점검")
-                            elif subitems[9].firstChild.nodeValue == 4:
-                                ChdataList.append("통신장애")
-                            elif subitems[9].firstChild.nodeValue == 5:
-                                ChdataList.append("통신미연결")
+                for i in item:
+                    if i.nodeName == "item":
+                        subitems = i.childNodes
+                        if self.Label1.get() in subitems[0].firstChild.nodeValue:
+                            pass
                         else:
                             continue
+                        if subitems[1].firstChild.nodeValue == '1':
+                            print(subitems[0].firstChild.nodeValue)
+                            if subitems[4].firstChild.nodeValue == '1':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "완속", "충전가능"))
+                            if subitems[4].firstChild.nodeValue == '2':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "완속", "충전중"))
+                            if subitems[4].firstChild.nodeValue == '3':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "완속", "고장/점검"))
+                            if subitems[4].firstChild.nodeValue == '4':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "완속", "통신장애"))
+                            if subitems[4].firstChild.nodeValue == '5':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "완속", "통신미연결"))
+                        if subitems[1].firstChild.nodeValue == '2':
+                            if subitems[4].firstChild.nodeValue == '1':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "고속", "충전가능"))
+                            if subitems[4].firstChild.nodeValue == '2':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "고속", "충전중"))
+                            if subitems[4].firstChild.nodeValue == '3':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "고속", "고장/점검"))
+                            if subitems[4].firstChild.nodeValue == '4':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "고속", "통신장애"))
+                            if subitems[4].firstChild.nodeValue == '5':
+                                ChdataList.append((subitems[0].firstChild.nodeValue, "고속", "통신미연결"))
+
+                    index += 1
 
                 for i in range(len(ChdataList)):
                     self.RenderText.insert(INSERT,"[")
-                    self.RenderText.insert(INSERT, "i+1")
+                    self.RenderText.insert(INSERT, i+1)
                     self.RenderText.insert(INSERT, "]")
-                    self.RenderText.insert(INSERT, "주소 : ")
+                    self.RenderText.insert(INSERT, " 주소 : ")
                     self.RenderText.insert(INSERT, ChdataList[i][0])
+                    self.RenderText.insert(INSERT, "\n")
                     self.RenderText.insert(INSERT, "충전기 타입 : ")
                     self.RenderText.insert(INSERT,ChdataList[i][1])
+                    self.RenderText.insert(INSERT, "\n")
                     self.RenderText.insert(INSERT, "충전기 상태 : ")
                     self.RenderText.insert(INSERT, ChdataList[i][2])
-
-
-
-
-
-
+                    self.RenderText.insert(INSERT, "\n")
 
     def InitGasStation(self):
         self.frame2 = Frame(window)
@@ -110,28 +120,139 @@ class MainGUI:
         self.Label2 = Entry(self.frame2)
         self.Label2.pack()
         self.Label2.place(x=10, y=30, width=200, height=30)
-        self.SearchButton2 = Button(self.frame2, text="검색")
+        self.SearchButton2 = Button(self.frame2, text="검색", command=self.SearchGasStation)
         self.SearchButton2.pack()
         self.SearchButton2.place(x=210, y=30, width=40, height=30)
-        self.listNodes2 = Listbox(self.frame2)
-        self.listNodes2.pack()
-        self.listNodes2.place(x=10, y=70, width=480, height=200)
+
+        self.RenderTextScrollbar2 = Scrollbar(self.frame2)
+        self.RenderTextScrollbar2.pack()
+        self.RenderTextScrollbar2.place(x=480, y=70)
+        self.RenderText2 = Text(self.frame2)
+        self.RenderText2.pack()
+        self.RenderText2.place(x=10, y=70)
+
+    def SearchGasStation(self):
+        #http://openapi.seoul.go.kr:8088/675a77514c6173683733696b55556c/xml/LOCALDATA_092808/1/1000/
+        import http.client
+        from xml.dom.minidom import parse, parseString
+        conn = http.client.HTTPConnection("openapi.seoul.go.kr:8088")
+        conn.request("GET", "/675a77514c6173683733696b55556c/xml/LOCALDATA_092808/1/1000/")
+        req = conn.getresponse()
+
+        GasdataList = []
+        GasdataList.clear()
+
+        if req.status == 200:
+            GasStationDoc = req.read().decode("utf-8")
+            if GasStationDoc == None:
+                print("ERROR")
+            else:
+                parseData = parseString(GasStationDoc)
+                all = parseData.childNodes
+                row = all[0].childNodes
+
+                for i in row:
+                    if i.nodeName == "row":
+                        subitems = i.childNodes
+                        if subitems[31].firstChild is not None:
+                            if self.Label2.get() in subitems[31].firstChild.nodeValue:
+                                pass
+                            else:
+                                continue
+                        if subitems[31].firstChild is not None:
+                            if subitems[37].firstChild is not None:
+                                if subitems[25].firstChild is not None:
+                                    tel = str(subitems[25].firstChild.nodeValue)
+                                    pass
+                                    if len(tel)<9:
+                                        tel = "02-"+tel
+                                        pass
+                                    GasdataList.append((subitems[37].firstChild.nodeValue,subitems[31].firstChild.nodeValue,tel))
+                                else:
+                                    GasdataList.append((subitems[37].firstChild.nodeValue,subitems[31].firstChild.nodeValue,"-"))
+
+                for i in range(len(GasdataList)):
+                    self.RenderText2.insert(INSERT,"[")
+                    self.RenderText2.insert(INSERT, i+1)
+                    self.RenderText2.insert(INSERT, "]")
+                    self.RenderText2.insert(INSERT, " 시설명 : ")
+                    self.RenderText2.insert(INSERT, GasdataList[i][0])
+                    self.RenderText2.insert(INSERT, "\n")
+                    self.RenderText2.insert(INSERT, "주소 : ")
+                    self.RenderText2.insert(INSERT,GasdataList[i][1])
+                    self.RenderText2.insert(INSERT, "\n")
+                    self.RenderText2.insert(INSERT, "전화 번호 : ")
+                    self.RenderText2.insert(INSERT, GasdataList[i][2])
+                    self.RenderText2.insert(INSERT, "\n")
 
     def InitNanumCar(self):
         self.frame3 = Frame(window)
         self.notebook.add(self.frame3, text="나눔카")
+
         self.Label3 = Entry(self.frame3)
         self.Label3.pack()
         self.Label3.place(x=10, y=30, width=200, height=30)
-        self.SearchButton3 = Button(self.frame3, text="검색")
+        self.SearchButton3 = Button(self.frame3, text="검색", command=self.SearchNanumCar)
         self.SearchButton3.pack()
         self.SearchButton3.place(x=210, y=30, width=40, height=30)
-        self.listNodes3 = Listbox(self.frame3)
-        self.listNodes3.pack()
-        self.listNodes3.place(x=10, y=70, width=480, height=200)
 
+        self.RenderTextScrollbar3 = Scrollbar(self.frame3)
+        self.RenderTextScrollbar3.pack()
+        self.RenderTextScrollbar3.place(x=480, y=70)
+        self.RenderText3 = Text(self.frame3)
+        self.RenderText3.pack()
+        self.RenderText3.place(x=10, y=70)
 
+    def SearchNanumCar(self):
+        #http://openapi.seoul.go.kr:8088/675a77514c6173683733696b55556c/xml/NanumcarSpotList/1/1000/
+        import http.client
+        from xml.dom.minidom import parse, parseString
+        conn = http.client.HTTPConnection("openapi.seoul.go.kr:8088")
+        conn.request("GET", "/675a77514c6173683733696b55556c/xml/NanumcarSpotList/1/1000/")
+        req = conn.getresponse()
 
+        NanumdataList = []
+        NanumdataList.clear()
 
+        if req.status == 200:
+            NanumDoc = req.read().decode("utf-8")
+            if NanumDoc == None:
+                print("ERROR")
+            else:
+                parseData = parseString(NanumDoc)
+                all = parseData.childNodes
+                row = all[0].childNodes
+
+                for i in row:
+                    if i.nodeName == "row":
+                        subitems = i.childNodes
+                        if subitems[9].firstChild is not None:
+                            if self.Label3.get() in subitems[9].firstChild.nodeValue:
+                                pass
+                            else:
+                                continue
+                        if subitems[9].firstChild is not None:
+                            if subitems[11].firstChild is not None:
+                                if subitems[7].firstChild is not None:
+                                    if subitems[7].firstChild.nodeValue == 'GA':
+                                        NanumdataList.append((subitems[11].firstChild.nodeValue,subitems[9].firstChild.nodeValue,"가솔린차"))
+                                    if subitems[7].firstChild.nodeValue == 'EV':
+                                        NanumdataList.append((subitems[11].firstChild.nodeValue,subitems[9].firstChild.nodeValue,"전기차"))
+                                    if subitems[7].firstChild.nodeValue == 'TO':
+                                        NanumdataList.append((subitems[11].firstChild.nodeValue,subitems[9].firstChild.nodeValue,"혼합차량"))
+
+                for i in range(len(NanumdataList)):
+                    self.RenderText3.insert(INSERT,"[")
+                    self.RenderText3.insert(INSERT, i+1)
+                    self.RenderText3.insert(INSERT, "]")
+                    self.RenderText3.insert(INSERT, " 거점명 : ")
+                    self.RenderText3.insert(INSERT, NanumdataList[i][0])
+                    self.RenderText3.insert(INSERT, "\n")
+                    self.RenderText3.insert(INSERT, "주소 : ")
+                    self.RenderText3.insert(INSERT,NanumdataList[i][1])
+                    self.RenderText3.insert(INSERT, "\n")
+                    self.RenderText3.insert(INSERT, "전기차 여부 : ")
+                    self.RenderText3.insert(INSERT, NanumdataList[i][2])
+                    self.RenderText3.insert(INSERT, "\n")
 
 MainGUI()
